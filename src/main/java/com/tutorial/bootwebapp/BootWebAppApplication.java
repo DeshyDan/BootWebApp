81ff97ec59e3952ac80beddd951061e7b0ea94ef
Deshy Dan <deshy733@gmail.com>
2024-06-20 08:57:06 +0200

    Implement task execution and scheduling

--- src/main/java/com/tutorial/bootwebapp/BootWebAppApplication.java
+++ src/main/java/com/tutorial/bootwebapp/BootWebAppApplication.java
@@ -1,10 +1,19 @@
 package com.tutorial.bootwebapp;
 
+import jdk.jfr.SettingDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.TimeUnit;
 
 
 @SpringBootApplication
+//@EnableScheduling
+//@EnableAsync
 public class BootWebAppApplication {
 
     public static void main(String[] args) {
@@ -12,4 +21,19 @@ public class BootWebAppApplication {
     }
 
 
+    //    @Scheduled(fixedRate = 5, timeUnit = TimeUnit.SECONDS)
+    //    @Async
+    //    public void sendEmails() throws InterruptedException {
+    //        System.out.println("Hello world");
+    //        Thread.sleep(5000);
+    //        System.out.println("Ending the world");
+    //    }
+    //
+    //    @Scheduled(cron = "*/5 * * * * *")
+    //    @Async
+    //    public void generateSalesEmail() throws InterruptedException {
+    //        System.out.println("start sales report");
+    //        Thread.sleep(5000);
+    //        System.out.println("end sales report");
+    //    }
 }
